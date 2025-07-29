153c9b9d38bac0caec3c7e518209d9b6263d4238
Basil ◡̈ <156693427+basil-boh@users.noreply.github.com>
2025-07-30 01:07:10 +0800

    feat: additional methods in playerService layer

--- src/main/java/com/pl/premier_zone/player/PlayerService.java
+++ src/main/java/com/pl/premier_zone/player/PlayerService.java
@@ -2,8 +2,10 @@ package com.pl.premier_zone.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component // Marks a Java class as a component
@@ -31,5 +33,51 @@ public class PlayerService {
             .stream()
             .filter(p -> p.getName().toLowerCase().contains(searchText.toLowerCase()))
             .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByPos(String searchText) {
+        return playerRepository.findAll()
+            .stream()
+            .filter(p -> p.getPos().toLowerCase().contains(searchText.toLowerCase()))
+            .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByNation(String searchText) {
+        return playerRepository.findAll()
+            .stream()
+            .filter(p -> p.getNation().toLowerCase().contains(searchText.toLowerCase()))
+            .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByTeamAndPosition(String team, String position) {
+        return playerRepository.findAll()
+            .stream()
+            .filter(p -> team.equals(p.getTeam()) && position.equals(p.getPos()))
+            .collect(Collectors.toList());
+    }
+
+    public Player addPlayer(Player player) {
+        playerRepository.save(player);
+        return player;
+    }
+
+    public Player updatePlayer(Player player) {
+        Optional<Player> existingPlayer = playerRepository.findByName(player.getName());
+        if (existingPlayer.isPresent()) {
+            Player playerToUpdate = existingPlayer.get();
+            playerToUpdate.setName(player.getName());
+            playerToUpdate.setNation(player.getNation());
+            playerToUpdate.setPos(player.getPos());
+            playerToUpdate.setTeam(player.getTeam());
+
+            playerRepository.save(playerToUpdate);
+            return playerToUpdate;
+        }
+        return null;
+    }
+
+    @Transactional // Maintains data consistency
+    public void deletePlayer(String playerName) {
+        playerRepository.deleteByName(playerName);
     }
 }
